126e0bad2c0c473a98f1d56696be7185d977a460
Divya Bharathi <divyaaa1812@gmail.com>
2018-03-17 08:53:10 -0700

    Refactoring reducers

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux'
+
 export const INITIAL_STATE = {
   selected: {
     userName: "",
@@ -8,37 +10,59 @@ export const INITIAL_STATE = {
   userDetails: {}
 };
 
-export const root = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
+export const selected = (state = INITIAL_STATE.selected, action) => {
+  switch(action.type) {
     case "UPDATE_DISPLAY_MODE": {
       return Object.assign({}, state, {
-        selected: Object.assign({}, state.selected, { displayMode: action.displayMode })
+        displayMode: action.displayMode
       });
     }
     case "UPDATE_SELECTED_USER": {
-      return Object.assign({}, state, { selected: { userName: action.userName, displayMode: "OVERVIEW" } });
+      return Object.assign({}, state, { userName: action.userName, displayMode: "OVERVIEW" });
     }
     case "FETCH_USER_DETAILS": {
-      return Object.assign({}, state, { selected: {
+      return Object.assign({}, state, {
         userName: action.userName,
         displayMode: "INIT",
         pageStatus: "FETCH_PENDING"
-      }});
+      });
     }
     case "ON_USER_DETAILS_FETCH": {
-      return Object.assign({},state, {
-        selected: Object.assign({}, state.selected, {
-          userName: action.userName ,
-          displayMode: "OVERVIEW",
-          pageStatus: "FETCH_COMPLETE"
-        }),
-        repos: Object.assign({}, state.repos, { [action.userName]: action.repos }),
-        userDetails: Object.assign({}, state.userDetails, { [action.userName]: action.userDetails })
+      return Object.assign({}, state, {
+        userName: action.userName ,
+        displayMode: "OVERVIEW",
+        pageStatus: "FETCH_COMPLETE"
       });
+    }
+    default:
+     return state;
+  }
+};
+
+export const repos = (state = INITIAL_STATE.repos, action ) => {
+  switch (action.type) {
+    case "ON_USER_DETAILS_FETCH": {
+      return Object.assign({}, state, { [action.userName]: action.repos })
     }
     default :
      return state;
   }
 };
+
+export const userDetails = (state=INITIAL_STATE.userDetails, action) => {
+  switch (action.type) {
+    case "ON_USER_DETAILS_FETCH": {
+      return Object.assign({}, state, { [action.userName]: action.userDetails });
+    }
+    default :
+     return state;
+  }
+};
+
+export const root = combineReducers({
+  selected,
+  repos,
+  userDetails
+});
 
 export default root;
